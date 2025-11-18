2337eb0094a107563df7b62eedb95698639333fa
Nawazishkarim07 <shiningnawaz123@gmail.com>
2025-11-18 16:53:38 +0530

    conversion operations in js

---
+++ 03_conversionOperations.js
@@ -0,0 +1,20 @@
+// // let value = 3
+// // let negValue = -value
+// // console.log(negValue);  //opertation on value
+
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2**3);
+// // console.log(2/2);
+// // console.log(2%3);
+
+// let str1 = "Nawazish"
+// let str2 = "Karim"
+// let str3 = str1+str2
+// console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);   // if the first value is string then it will comvert expression to string..
+console.log(1+2+"2");   // if the first value is number and last is string then only it first adds it then convert it to string
