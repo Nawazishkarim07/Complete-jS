a9ab239e155e025f33b3a58da5b414868c007734
Nawazishkarim07 <shiningnawaz123@gmail.com>
2025-11-18 17:10:02 +0530

    comparison in js

---
+++ 04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);     //it converts string to number
+
+// console.log(null>0);    // null gets converted to 0
+
+// console.log(null == 0);     // == doesnt convert it to number
+
+// console.log(undefined == 0);
+
+// === strict check not only comapres value but also data type
+
+console.log("2"===2);   // not equal as string and numbers are not same data type
+
+
