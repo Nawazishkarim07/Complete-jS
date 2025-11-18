7afb90470de6737c0e81daad39333978c974871c
Nawazishkarim07 <shiningnawaz123@gmail.com>
2025-11-18 20:28:29 +0530

    Maths and Numbers in js

---
+++ 07_numsAndMaths.js
@@ -0,0 +1,27 @@
+// let score = 400.3424
+// console.log(score);
+
+// let balance = new Number(100)
+// console.log(balance);
+
+// console.log(score.toString().length)
+// console.log(score.toFixed(2))
+// console.log(score.toPrecision(3))
+
+// const amount = 10000000
+// console.log(amount.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++++ MATHS IN JS +++++++++++++++++++++++ 
+
+// console.log(Math);  // it is an object in js
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.33232))
+// console.log(Math.ceil(4.1)) // always increase 
+// console.log(Math.floor(4.7)) //always decrease to base
+// console.log(Math.min(4,2,1,55)) //gives lowest value of the array
+// console.log(Math.max(4,3,2,55)) //gives the max value of the array
+
+console.log(Math.random())      //gives random value between 0 and 1
+const min = 10
+const max  = 20
+console.log(Math.round(Math.random()*(max-min+1)+min))
