1df7d39ee38880b21643613fc3bc80301ea8fcac
Nawazishkarim07 <shiningnawaz123@gmail.com>
2025-11-23 16:51:06 +0530

    About date and time in js

--- 08_dateAndTime.js
+++ 08_dateAndTime.js
@@ -1,13 +1,13 @@
-// let myDate = new Date()
-// console.log(myDate.toString())
-// console.log(myDate.toDateString())
-// console.log(myDate.toISOString())
-// console.log(myDate.toLocaleDateString())
-// console.log(myDate.toLocaleString())
-// console.log(myDate.toLocaleTimeString())
-// console.log(myDate.toTimeString())
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toTimeString())
 
 let createdDate = new Date(2023,0,12,5,3)
 console.log(createdDate.toLocaleString());
-let mytimeStamp = Date.now()
+let mytimeStamp = Date.now()    // it gives timestamp in non readable format
 console.log(mytimeStamp.toLocaleString())
