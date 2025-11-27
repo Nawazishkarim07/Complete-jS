3e2fa013dc8e67276ede6a99cee21516d3ba1222
Nawazishkarim07 <shiningnawaz123@gmail.com>
2025-11-27 22:31:16 +0530

    control flow in js

---
+++ 16_controlFlow.js
@@ -0,0 +1,53 @@
+// const month = 3;
+// switch(month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feburary");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("June");
+//         break;
+//     case 7:
+//         console.log("July");
+//         break;
+//     case 8:
+//         console.log("August");
+//         break;
+//     case 9:
+//         console.log("September");
+//         break;
+//     case 10:
+//         console.log("October");
+//         break;
+//     case 11:
+//         console.log("November");
+//         break;
+//     case 12:
+//         console.log("December");
+//         break;
+//     default:
+//         console.log("You go tyour month");
+//         break;
+// }
+
+// Nullish Coalascing operator...
+
+val1 = null ?? 10           // use to handle with null and undefined values..
+console.log(val1);
+
+// Terniary operator..
+// condition ? true : false;
+
+const iceTeaPrice = 199
+iceTeaPrice >= 100 ? console.log("Price more than 100") : console.log("price less than 100")
