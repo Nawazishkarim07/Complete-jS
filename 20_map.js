fed2b31e6aaac13b079c72ad2557167d7489aae0
Nawazishkarim07 <shiningnawaz123@gmail.com>
2025-12-05 22:11:26 +0530

    Understanding maps

--- 20_map.js
+++ 20_map.js
@@ -1,4 +1,5 @@
 const arr = [1,2,3,4,5,6,7,8,9]
 //const newArr = arr.map((num)=> num+10)
-const newArr = arr.map((nums)=>nums+10).map((nums)=>nums-12).filter((nums)=>nums>0)
-console.log(newArr);
+//const newArr = arr.map((nums)=>nums+10).map((nums)=>nums-12).filter((nums)=>nums>0)
+const arr2 = arr.map((nums)=>nums+1).map((nums)=>nums+2).filter((nums)=>nums>10)
+console.log(arr2);
